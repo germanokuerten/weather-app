fb35cf9b8c8ee9dc2b145149679fce1ac57d30ee
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-03-23 14:28:06 -0700

    api's working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-const APIKEY = "d5a2d9bc210f3ec1e02af0da5fa7e705"
-
-
+// const APIKEY = "d5a2d9bc210f3ec1e02af0da5fa7e705";
 
 
 const $city = $("#city")
@@ -8,9 +6,56 @@ const $temperature = $("#temperature")
 const $feelsLike = $("#feels-like")
 const $weather = $("#weather")
 
+const $input = $("input")
+const $button = $("button")
 
 
-$.ajax({
-    url: `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${APIKEY}`
+$button.on("click", () => {
+    const searchCity = $input.val()
+
+    // be able to search input data from searchCity
+    $.ajax(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity},&appid=d5a2d9bc210f3ec1e02af0da5fa7e705`)
+    .then((coordinates) => {
+        console.log(coordinates)
+
+    // grab lat and lon from searchCity.
+
+    const lat = coordinates[0].lat;
+    const lon = coordinates[0].lon;
+
+    // chech lat and lon
+
+    console.log(lat, lon)
+
+    // then throw into api address
+
+    $.ajax(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d5a2d9bc210f3ec1e02af0da5fa7e705`)
+    .then((data) => {
+        // test data input/output
+        console.log(data)
+        // $("#city").html(`<p>${data.name}</p>`)
+    })
+    
+    // output the results /information
+    
+
+
+
+
+    // convert lan and lon into the data we want.
+
+    
+
+    
+
+    
+    // city = data.name
+    // temperature = data.main.temp
+    // feels like = data.main.feels_like
+    // weather = data.weather[1]
+
+    // output next to parameters.
+
+    })
 })
 
